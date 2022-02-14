4f2cec178c2273b08e4dbd4519600257cbde9267
Daksh <dpatel132@hawk.iit.edu>
2022-02-14 16:52:30 -0600

    Initial Commit

---
+++ Shared/Quantum_1S_IntegralApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quantum_1S_IntegralApp.swift
+//  Shared
+//
+//  Created by Daksh Patel on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Quantum_1S_IntegralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
